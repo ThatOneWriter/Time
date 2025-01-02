16b774a1cd7aaeed4cc0824cc7761d6d6db7ac6d
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-01-01 21:00:48 -0500

    Time.java
    Added javadocs

--- Time.java
+++ Time.java
@@ -1,3 +1,7 @@
+/**
+ * The Time class represents the time in a 24-digital hour clock setting in military time.
+ * Time contains a set amount of hours, minutes, and seconds.
+ */
 public class Time {
 
 
@@ -5,7 +9,15 @@ public class Time {
    private int minutes;
    private int seconds;
 
-
+ /**
+     * Constructor for the Time class. This creates an instance of the time given the
+     * parameters mentioned below.
+     *
+     * @param hours represents the exact hour the time is set at.
+     * @param minutes represents the exact minute the time is set at.
+     * @param seconds represents the the exact second the time is set at.
+     *
+     */
    public Time(int hours, int minutes, int seconds) {
        this.hours = hours;
        this.minutes = minutes;
@@ -14,7 +26,10 @@ public class Time {
 
    }
 
-
+   /**
+     * The tick method for the Time class
+     * This method adds 1 second each time it is called, and properly adjusts the time if the amount of seconds/minutes is > 60 and if hours > 23
+     */
    public void tick() {
        seconds++;
        if (seconds >= 59) {
@@ -30,7 +45,13 @@ public class Time {
        }
    }
 
-
+     /**
+     * the add method for the Time class.
+     * This method will return an int displaying the sum of the two times.
+     *
+     * @return returns the sum.
+     *
+     */
    public int add(int t) {
        seconds += t;
       if (seconds >= 60){
@@ -43,7 +64,13 @@ public class Time {
        return (seconds + minutes * 60 + hours * 3600);
    }
 
-   
+    /**
+     * toString method for the Time class. This method will return a String
+     * which displays the time in this format: HH:MM:SS.
+     *
+     * @return returns the appropriate formatted time.
+     *
+     */
    public String toString() {
        String str = "";
        if (hours < 10) {
